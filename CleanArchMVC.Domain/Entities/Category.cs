f50da0806439cdba2487fdc6767da8ff1cb59f87
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-07 20:22:22 -0300

    Enriquecendo o modelo de dominio

--- CleanArchMVC.Domain/Entities/Category.cs
+++ CleanArchMVC.Domain/Entities/Category.cs
@@ -1,12 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMVC.Domain.Validation;
+using System;
+using System.Collections.Generic;
 
 namespace CleanArchMVC.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
+        public int Id { get; private set; }
+        public string Name { get; private set; }
         public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidationDomain(name);
+        }
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), $"Invalid {name}.Name is Required");
+            DomainExceptionValidation.When(name.Length < 3, $"Invalid name, too short, minumum 3 Caracteres");
+
+            name = Name;
+        }
     }
 }
