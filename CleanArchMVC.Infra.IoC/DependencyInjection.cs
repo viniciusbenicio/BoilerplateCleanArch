a3d5f62dfc70acc552bece4798b070b9d7e55de7
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-15 22:17:46 -0300

    Criando Controller e View de Categories e Products

--- CleanArchMVC.Infra.IoC/DependencyInjection.cs
+++ CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using CleanArchMVC.Domain.Interfaces;
+﻿using CleanArchMVC.Application.Interfaces;
+using CleanArchMVC.Application.Mappings;
+using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Interfaces;
 using CleanArchMVC.Infra.Data.Context;
 using CleanArchMVC.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +18,9 @@ namespace CleanArchMVC.Infra.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;
         }
---
+++ CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMVC.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productsService;
+        public ProductsController(IProductService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productsService.GetProducts();
+            return View(products);
+        }
+    }
+}
