7133e7a43a33d5d1f5fd76894ba81921826c3bcd
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-18 23:02:33 -0300

    Controller Home, Roteamento, View e Layout

--- CleanArchMVC.Application/Products/Handlers/GetProductByIdQueryHandler.cs
+++ CleanArchMVC.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace CleanArchMVC.Application.Products.Handlers
 {
-    public class GetProductByIdQueryHandler : IRequestHandler<GetProductsByIdQuery, Product>
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
     {
         private readonly IProductRepository _productRepository;
         public GetProductByIdQueryHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
-        public async Task<Product> Handle(GetProductsByIdQuery request, CancellationToken cancellationToken)
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
         {
            return await _productRepository.GetByIdAsync(request.Id);
         }
---
+++ CleanArchMVC.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
