b3106a6a41c275ab28e409fad9ec1922e520f95e
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-21 23:25:00 -0300

    Criando a camada de WebAPI para ser consumido.

---
+++ CleanArchMVC.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMVC.Application.Interfaces;
+using CleanArchMVC.Application.Mappings;
+using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Account;
+using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using CleanArchMVC.Infra.Data.Identity;
+using CleanArchMVC.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace CleanArchMVC.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMVC.Application");
+            services.AddMediatR(myhandlers);
+
+            return services;
+        }
+    }
+}
