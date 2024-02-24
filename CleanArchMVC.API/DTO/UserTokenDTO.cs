6df35a4a5780f399e056506b37715901674fcd66
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-23 22:00:04 -0300

    Autenticação Jwt Criação de Usuário e geração de token

---
+++ CleanArchMVC.API/DTO/UserTokenDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CleanArchMVC.API.DTO
+{
+    public class UserTokenDTO
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
