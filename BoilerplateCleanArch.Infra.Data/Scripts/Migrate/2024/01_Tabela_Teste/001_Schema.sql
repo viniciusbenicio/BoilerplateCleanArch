cad54cb35a595e938a08cf205bf8734cde7d1ba6
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-27 20:41:38 -0300

    fix scripts and folders

--- BoilerplateCleanArch.Infra.Data/Scripts/Migrate/2024/01_Tabela_Teste/001_Schema.sql
+++
@@ -1,8 +0,0 @@
-﻿IF Object_Id('tb_teste') IS NULL 
-BEGIN
-  CREATE TABLE tb_teste
-  (
-   Id INT PRIMARY KEY,
-   Nome VARCHAR(30)
-  );
-END
--- BoilerplateCleanArch.Infra.Data/Scripts/Migrate/2024/01_Tabela_Teste/002_Populate.sql
+++
@@ -1,5 +0,0 @@
-﻿IF NOT EXISTS (SELECT 1 FROM tb_teste WITH (NOLOCK) WHERE Nome = 'vinicius')
-BEGIN
-    INSERT INTO tb_teste VALUES (1, 'vinicius');
-END
-GO
