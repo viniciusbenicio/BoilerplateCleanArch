cfaccd78cfa5c3c94998f9c5f2d1a2bd90533d0b
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-26 23:36:37 -0300

    Utilizando DbUp para criação das tabelas, procedure tudo manualmente e execução posterior dos scirpts.

---
+++ BoilerplateCleanArch.Infra.Data/Scripts/Migrate/01_Tabela_Teste/001_Schema.sql
@@ -0,0 +1,8 @@
+﻿﻿If Object_Id('tb_teste') Is Null 
+BEGIN
+  CREATE TABLE tb_teste
+  (
+   Id INT  PRIMARY KEY,
+   Nome VARCHAR(30)
+  )
+END
---
+++ BoilerplateCleanArch.Infra.Data/Scripts/Migrate/01_Tabela_Teste/002_Populate.sql
@@ -0,0 +1,6 @@
+﻿﻿If Not Exists (Select 1 From tb_teste (Nolock) Where Nome = 'vinicius') 
+Begin
+	
+	INSERT INTO tb_teste VALUES (1,'vinicius');
+End
+GO
