317196281eb8cefb7f31369e2073dc95c1de60de
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-02-28 22:59:41 -0300

    When not to have the database The script will create it inform the database in ""Database=name Database"

--- BoilerplateCleanArch.Infra.Data/Program.cs
+++ BoilerplateCleanArch.Infra.Data/Program.cs
@@ -6,12 +6,9 @@ class Program
     static int Main(string[] args)
     {
         var connectionString = args.FirstOrDefault() ?? "Server=localhost,1433;Database=appClean;User ID=app;Password=123@123@;TrustServerCertificate=True";
-
-
+        EnsureDatabase.For.SqlDatabase(connectionString);
         var upgrader = DeployChanges.To.SqlDatabase(connectionString).WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly()).LogToConsole().Build();
-
         var result = upgrader.PerformUpgrade();
-
 
         if (!result.Successful)
         {
@@ -27,9 +24,7 @@ class Program
         Console.ForegroundColor = ConsoleColor.Green;
 
         foreach (var script in result.Scripts)
-        {
             Console.WriteLine($" Executado: {script.Name}");
-        }
 
         Console.ResetColor();
         return 0;
